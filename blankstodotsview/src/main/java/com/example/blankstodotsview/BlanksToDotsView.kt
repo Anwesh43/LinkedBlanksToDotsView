1f98d9530688e0c2bfdeb9a8bd3f4068243b18a9
Anwesh43 <anweshthecool0@gmail.com>
2021-06-22 00:43:49 +0530

    adding Canvas extension methods

--- blankstodotsview/src/main/java/com/example/blankstodotsview/BlanksToDotsView.kt
+++ blankstodotsview/src/main/java/com/example/blankstodotsview/BlanksToDotsView.kt
@@ -19,7 +19,7 @@ val colors : Array<Int> = arrayOf(
 }.toTypedArray()
 val lines : Int = 4
 val parts : Int = 3
-val scGap : Float = 0.12f / (parts * lines)
+val scGap : Float = 0.13f / (parts * lines)
 val strokeFactor : Float = 90f
 val rFactor : Float = 29.3f
 val delay : Long = 20
@@ -30,3 +30,41 @@ val rot : Float = 180f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawBlanksToDots(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val gap : Float = w / parts
+    val lGap : Float = gap / (2 * lines)
+    val r : Float = Math.min(w, h) / rFactor
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..(lines - 1)) {
+        val sc1j : Float = sc1.divideScale(j, lines)
+        val sc2j : Float = sc2.divideScale(j, lines)
+        val sc3j : Float = sc3.divideScale(j, lines)
+        save()
+        translate(-gap / 2 + lGap * (2 * j), 0f)
+        drawLine(
+            lGap * sc2j,
+            0f,
+            lGap * sc1j - lGap * 0.5f * sc2j,
+            0f,
+            paint
+        )
+        drawCircle(lGap / 2, (h / 2 + r) * sc3j, r * sc2j, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawBTDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBlanksToDots(scale, w, h, paint)
+}
