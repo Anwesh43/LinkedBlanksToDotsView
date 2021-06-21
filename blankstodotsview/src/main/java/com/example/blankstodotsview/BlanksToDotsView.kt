2f7cee401a611363eade3b15dfc2444b5259d30d
Anwesh43 <anweshthecool0@gmail.com>
2021-06-22 00:33:07 +0530

    adding constants and libraries

--- blankstodotsview/src/main/java/com/example/blankstodotsview/BlanksToDotsView.kt
+++ blankstodotsview/src/main/java/com/example/blankstodotsview/BlanksToDotsView.kt
@@ -1,1 +1,27 @@
 package com.example.blankstodotsview
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+
+val colors : Array<Int> = arrayOf(
+    "#f44336",
+    "#004D40",
+    "#FFD600",
+    "#00C853",
+    "#6200EA"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val lines : Int = 4
+val parts : Int = 3
+val scGap : Float = 0.12f / (parts * lines)
+val strokeFactor : Float = 90f
+val rFactor : Float = 29.3f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val sizeFactor : Float = 3f
