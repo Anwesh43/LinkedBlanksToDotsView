478ad40c75650861b465abf80f8f0c4d2519dcf3
Anwesh43 <anweshthecool0@gmail.com>
2021-06-22 01:08:49 +0530

    adding fullscreen and view

--- app/src/main/java/com/example/linkedblankstodotsview/MainActivity.kt
+++ app/src/main/java/com/example/linkedblankstodotsview/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.linkedblankstodotsview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
